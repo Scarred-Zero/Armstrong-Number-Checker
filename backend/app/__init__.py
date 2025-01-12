67d83055a2fe16f000a8574379e4c36f86b12bcb
Scarred-Zero <danemmanuel441@gmail.com>
2025-01-12 21:53:31 +0100

    Initial commit

---
+++ backend/app/__init__.py
@@ -0,0 +1,73 @@
+from flask import Flask, render_template, flash, redirect, url_for, request
+from flask_login import LoginManager
+from os import path
+from .config.database import db
+from .models.Models import User
+from .config.variables import SECRET_KEY, MYSQL_HOST, MYSQL_PORT, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DB, DATABASE_URI
+
+
+def create_app():
+    app = Flask(__name__)
+
+    # CONFIGS
+    app.config['SECRET_KEY'] = SECRET_KEY
+    app.config['MYSQL_HOST'] = MYSQL_HOST
+    app.config['MYSQL_PORT'] = MYSQL_PORT
+    app.config['MYSQL_USER'] = MYSQL_USER
+    app.config['MYSQL_PASSWORD'] = MYSQL_PASSWORD
+    app.config['MYSQL_DB'] = MYSQL_DB
+    app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URI
+
+    # Blueprint for authentication
+    from .views.auth import auth
+    app.register_blueprint(auth, url_prefix='/auth')
+
+    # Blueprint for Armstrong Number Checker
+    from .views.app_routes import arm_num_checker
+    app.register_blueprint(arm_num_checker, url_prefix='/arm-num-checker')
+
+    # Blueprint for user management
+    from .views.user import user_bluprt
+    app.register_blueprint(user_bluprt, url_prefix='/user')
+
+    # Other setups
+    db.init_app(app)
+
+    # Models
+    from . import models
+
+    create_database(app)
+
+    # Login Manager
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login_page'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(usr_id):
+        return User.query.get(usr_id)
+
+    @login_manager.unauthorized_handler
+    def handle_needs_login():
+        flash("You have to be logged in to access this page.")
+        return redirect(url_for('auth.login_page', next=request.endpoint))
+
+    # Error handling
+    @app.errorhandler(404)
+    def page_not_found(error):
+        print("404 ERROR:", str(error))
+        return render_template("errors/error-404.html")
+
+    @app.errorhandler(Exception)
+    def server_error(error):
+        print("SERVER ERROR:", str(error))
+        return render_template("errors/server-error.html")
+
+    return app
+
+
+def create_database(app):
+    if not path.exists('app/' + MYSQL_DB):
+        with app.app_context():
+            db.create_all()
+            print(' *', 'Database created and tables initialized!')
---
+++ backend/app/views/app_routes.py
@@ -0,0 +1,165 @@
+from flask import (
+    Blueprint,
+    render_template,
+    request,
+    redirect,
+    url_for,
+    flash,
+    session,
+    jsonify,
+)
+from flask_login import login_required, current_user
+from ..models.Models import User, Feedback
+from ..config.database import db
+from .forms import ArmstrongForm, CheckNumberForm, FeedbackForm
+
+# CREATE A BLUEPRINT FOR THE ARMSTRONG NUMBER CHECKER
+arm_num_checker = Blueprint("arm_num_checker", __name__)
+
+
+# FUNCTION TO CHECK IF NUMBER IS AN ARMSTRONG NUMBER
+def is_armstrong(number):
+    num_str = str(number)
+    num_digits = len(num_str)
+    total = sum(int(digit) ** num_digits for digit in num_str)
+    return total == number
+
+
+# ROUTE FOR HOME PAGE
+@arm_num_checker.route("/", methods=["GET", "POST"])
+# @arm_num_checker.route('/home', methods=['GET', 'POST'])
+@login_required
+def home_page():
+    form_data = ArmstrongForm()
+    form_data_2 = CheckNumberForm()
+
+    if request.method == "POST":
+        if form_data.validate_on_submit():
+            if int(form_data.min_num.data) > int(form_data.max_num.data):
+                return (
+                    jsonify(
+                        {
+                            "error": "Minimum number must be less than or equal to maximum number."
+                        }
+                    ),
+                    400,
+                )
+            if int(form_data.min_num.data) < 1 or int(form_data.max_num.data) < 1:
+                return (
+                    jsonify(
+                        {
+                            "error": "Please enter positive integers for minimum and maximum numbers."
+                        }
+                    ),
+                    400,
+                )
+
+            min_num = int(form_data.min_num.data)
+            max_num = int(form_data.max_num.data)
+
+            # Calculate Armstrong numbers
+            armstrong_numbers = [
+                number for number in range(min_num, max_num + 1) if is_armstrong(number)
+            ]
+            if not armstrong_numbers:
+                return (
+                    jsonify(
+                        {"error": "No Armstrong numbers found within the given range."}
+                    ),
+                    400,
+                )
+
+            # Store in session
+            session["armstrong_numbers"] = armstrong_numbers
+
+            return jsonify({"redirect": url_for("arm_num_checker.results_page")})
+    # FOR CHECKING A SPECIFIC ARMSTRONG NUMBER
+    if form_data_2.validate_on_submit():
+        check_particular_num = int(form_data_2.check_particular_num.data)
+        if check_particular_num < 1:
+            return (
+                jsonify(
+                    {
+                        "error": "Please enter a positive integer for the number to check."
+                    }
+                ),
+                400,
+            )
+
+        # HANDLE FORM SUBMISSION
+        result = (
+            f"{check_particular_num} is an Armstrong Number"
+            if is_armstrong(check_particular_num)
+            else f"{check_particular_num} is not an Armstrong Number"
+        )
+
+        # STORE THE RESULTS IN THE SESSION
+        session["check_num_result"] = result
+        return jsonify({"redirect": url_for("arm_num_checker.results_page")})
+
+    return render_template(
+        "index.html",
+        form=form_data,
+        form2=form_data_2,
+        title="Home | Armstrong Number Checker",
+    )
+
+
+# ROUTES FOR DISPLAYING RESULTS
+@arm_num_checker.route("/results", methods=["GET"])  # <usr_id>
+@login_required
+def results_page():
+    # RETRIEVE THE RESULT FROM THE SESSION
+    armstrong_numbers = session.get('armstrong_numbers')
+    result = session.get('check_num_result')
+
+    return render_template(
+        "results.html",
+        armstrong_number=armstrong_numbers,
+        result=result,
+        title="Results | Armstrong Number Checker",
+    )
+
+
+# ROUTES FOR CONTACT PAGE
+@arm_num_checker.route("/contact_submit_feedback/<usr_id>", methods=["GET", "POST"])
+@login_required
+def contact_page(usr_id):
+    user = User.query.filter_by(usr_id=usr_id).first()
+    form_data = FeedbackForm(obj=user)
+    if request.method == "POST":
+        if form_data.validate_on_submit():
+            name = form_data.name.data
+            email = form_data.email.data
+            subject = form_data.subject.data
+            message = form_data.message.data
+
+            feedback = Feedback(
+                usr_id=current_user.usr_id,
+                name=name,
+                email=email,
+                subject=subject,
+                message=message,
+            )
+
+            try:
+                # SAVE THE FEEDBACK TO THE DATABASE
+                db.session.add(feedback)
+                db.session.commit()
+                flash(
+                    "Your message was sent successfully. Thank you for your feedback!",
+                    category="success",
+                )
+                return redirect(
+                    url_for("arm_num_checker.contact_page", usr_id=user.usr_id)
+                )
+            except Exception as e:
+                db.session.rollback()
+                flash(f"Error submitting feedback: {e}", category="error")
+                return redirect(
+                    url_for("arm_num_checker.contact_page", usr_id=user.usr_id)
+                )
+
+    return render_template(
+        "contact.html", form=form_data, title="Contact Us | Armstrong Number Checker"
+    )
