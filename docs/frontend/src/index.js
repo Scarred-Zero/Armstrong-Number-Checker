a369259eed73647ce2e1c3043aaae2eddaecdee1
Scarred-Zero <danemmanuel441@gmail.com>
2025-01-15 06:55:10 +0100

    Initial commit

--- docs/frontend/src/index.js
+++ docs/frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { AlertProvider } from '../src/components/alert/AlertContext';
+import { AlertProvider } from './components/alert/AlertContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
